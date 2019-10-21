8fc82b9029e99354787877cf12c37607d8998302
Kawsar Hussen <kawsarhussen@gmail.com>
2019-10-21 16:25:46 -0400

    Login endpoint created

--- routers/login.js
+++ routers/login.js
@@ -1,5 +1,31 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const Employee = require('../data/employeeModel.js');
+const bcrypt = require('bcryptjs');
+const { generateToken } = require('../auth/auth.js');
 
 router.post('/', (req, res) => {
-    res.status(200).json({ message: `Welcome` });
-})
+    try {
+        let { username, password } = req.body;
+        if (!username || !password) {
+            res.status(400).json({ Message: "Please provide both username and password" })
+        } else {
+            Employee.findBy({ username })
+                .first()
+                .then(user => {
+                    console.log(user)
+                    if ((user && (password === user.password) || bcrypt.compareSync(password, user.password))) {
+                        const token = generateToken(user);
+                        res.status(200).json({ message: `Welcome ${user.username}!`, token, id: user.id, });
+                    } else {
+                        res.status(401).json({ message: 'Invalid Credentials' });
+                    }
+                })
+        }
+    } catch (error) {
+        res.status(500).json({ message: `An error occured : ${error}` });
+    };
+});
+
+
+module.exports = router;

de3a061d2c64cb68b4f095398d3fa12ae03f3535
Kawsar Hussen <kawsarhussen@gmail.com>
2019-10-21 19:27:04 -0400

    Client endoint added

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 
 const register = require('../routers/register.js');
 const login = require('../routers/login.js');
+const clients = require('../routers/clients.js')
 const server = express();
 
 server.use(express.json());
@@ -14,10 +15,12 @@ server.use(cors());
 
 server.use('/api/login', login);
 server.use('/api/register', register);
-
+server.use('/api/client', clients);
 
 server.get('/', (req, res) => {
     res.send('server is up');
+    console.log(req.body)
 });
+
 
 module.exports = server;

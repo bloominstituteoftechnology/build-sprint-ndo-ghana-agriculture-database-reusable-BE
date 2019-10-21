eeb9b2acda7c40763d783b8ff790bc2614b3d0ae
Kawsar Hussen <kawsarhussen@gmail.com>
2019-10-21 14:25:35 -0400

    Authentication implemented to authenticate a user

---
+++ data/employeeModel.js
@@ -0,0 +1,50 @@
+const db = require('./dbConfig.js');
+
+module.exports = {
+    add,
+    addUser,
+    find,
+    findBy,
+    findById,
+    update,
+    deleteUser,
+};
+
+function update(id, user) {
+    return db('employee')
+        .where('id', Number(id))
+        .update(user);
+}
+
+
+function deleteUser(id) {
+    return db('employee')
+        .where({ id })
+        .delete()
+
+}
+
+function find() {
+    return db('employee').select('id', 'username');
+}
+
+function findBy(filter) {
+    return db('employee').where(filter);
+}
+
+async function add(user) {
+    const [id] = await db('employee').insert(user)
+        .returning("id");
+    return findById(id);
+}
+async function addUser(user) {
+    const [id] = await db('employee').insert(user)
+    //.returning("id");
+    return findById(id);
+}
+
+function findById(id) {
+    return db('employee')
+        .where({ id })
+        .first();
+}

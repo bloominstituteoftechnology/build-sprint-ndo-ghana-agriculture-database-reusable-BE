15080cb2c028cce155d56cc8904d7bdfa6d7c335
Kawsar Hussen <kawsarhussen@gmail.com>
2019-10-24 14:40:06 -0400

    Origin access error problem trying to fix

--- api/server.js
+++ api/server.js
@@ -12,7 +12,11 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 server.use('/api/login', login);
 server.use('/api/register', register);
